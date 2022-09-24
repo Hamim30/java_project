29132048b51c9ade843be2047b92e291516dde93
Hamim30 <hamimnasim30@gmail.com>
2022-09-24 17:32:49 +0600

    Sum product difference

---
+++ src/SumProductDifference.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class SumProductDifference {
+    public static void main(String[] args) {
+        Scanner inputTake = new Scanner(System.in);
+        int one= inputTake.nextInt();
+        int two= inputTake.nextInt();
+        System.out.println(one+two);
+        System.out.println(one*two);
+        System.out.println(one-two);
+
+    }
+}
