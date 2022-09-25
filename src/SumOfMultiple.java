951ab46718bb331b85e3b649885499ea8d41cc36
Hamim30 <hamimnasim30@gmail.com>
2022-09-25 21:08:38 +0600

    another task

--- src/SumOfMultiple.java
+++ src/SumOfMultiple.java
@@ -2,14 +2,19 @@ public class SumOfMultiple {
     public static void main(String[] args) {
         int sum=0;
         int sumTwo=0;
+        int sumThree=0;
         for (int i =0; i<=600;i++){
             if(i % 7==0 && i%9==0){
                 sum+=i;
-            } if (i % 7==0 || i%9==0) {
+            }else if (i % 7==0 || i%9==0) {
+                sumThree+=i;
+            }
+            if (i % 7==0 || i%9==0) {
                 sumTwo+=i;
             }
         }
         System.out.println(sum);
         System.out.println(sumTwo);
+        System.out.println(sumThree);
     }
 }
