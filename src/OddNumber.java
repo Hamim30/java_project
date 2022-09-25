0ce71012f4526048801f9208c3c5adf4ddf0ecce
Hamim30 <hamimnasim30@gmail.com>
2022-09-25 21:19:25 +0600

    odd number in a range

---
+++ src/OddNumber.java
@@ -0,0 +1,9 @@
+public class OddNumber {
+    public static void main(String[] args) {
+        for(int i=10; i<=50 ; i++){
+            if (i%2==1){
+                System.out.print(i+ " ");
+            }
+        }
+    }
+}
