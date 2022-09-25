0e04a062957e809af1f442232f247b475d58eb94
Hamim30 <hamimnasim30@gmail.com>
2022-09-25 20:44:36 +0600

    Pattern task

---
+++ src/PatternSC.java
@@ -0,0 +1,9 @@
+public class PatternSC {
+    public static void main(String[] args) {
+        int one = 24;
+        while (one>=-6){
+            System.out.print(one+" ");
+            one -=6;
+        }
+    }
+}
