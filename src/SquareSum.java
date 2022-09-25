c0d243ee8f31c9f4ecb5cf956b43e873b2117925
Hamim30 <hamimnasim30@gmail.com>
2022-09-25 21:19:58 +0600

    Square Task

--- src/SquareSum.java
+++ src/SquareSum.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class SquareSum {
+import java.util.Scanner;
+public class SquareSum {
+    public static void main(String[] args) {
+        Scanner inputTake= new Scanner(System.in);
+        int number = inputTake.nextInt();
+        int sum=0;
+        for (int i=1; i<=number; i++){
+            if (i%2==0){
+                sum-=Math.pow(i,2);
+            }else {
+                sum+=Math.pow(i,2);
+            }
+        }
+        System.out.println(sum);
+    }
 }
