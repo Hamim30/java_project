de2146d5cfb2c81ff63df925ae07c12855639ef7
Hamim30 <hamimnasim30@gmail.com>
2022-09-25 21:06:39 +0600

    additonal task

--- src/SumOfMultiple.java
+++ src/SumOfMultiple.java
@@ -1,11 +1,15 @@
 public class SumOfMultiple {
     public static void main(String[] args) {
         int sum=0;
+        int sumTwo=0;
         for (int i =0; i<=600;i++){
             if(i % 7==0 && i%9==0){
                 sum+=i;
+            } if (i % 7==0 || i%9==0) {
+                sumTwo+=i;
             }
         }
         System.out.println(sum);
+        System.out.println(sumTwo);
     }
 }
