24e3ce5b5b53b4675a75f63f0e0676635d9613c6
Hamim30 <hamimnasim30@gmail.com>
2022-09-26 21:06:11 +0600

    divisor

--- src/Divisors.java
+++ src/Divisors.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Divisors {
+import java.util.Scanner;
+
+public class Divisors {
+    public static void main(String[] args) {
+        Scanner inputTake = new Scanner(System.in);
+        int number = inputTake.nextInt();
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) {
+                System.out.println(i);
+            }
+        }
+    }
 }
