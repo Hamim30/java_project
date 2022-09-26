707b2e2cf54f7bc58c5606edd97a91e6d4377438
Hamim30 <hamimnasim30@gmail.com>
2022-09-26 16:30:50 +0600

    Additional task

--- src/DigitExtract.java
+++ src/DigitExtract.java
@@ -3,10 +3,14 @@ public class DigitExtract {
     public static void main(String[] args) {
         Scanner inputTake = new Scanner(System.in);
         int user = inputTake.nextInt();
+        int count=0;
         while ((user%10)!=0){
             int digit= user %10;
             System.out.print(digit+ " ");
             user= user/10;
+            count ++;
         }
+        System.out.println();
+        System.out.println("There are "+count+" Digit");
     }
 }
