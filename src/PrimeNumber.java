71b96635977a0572b7c3a538210b749aabad72ba
Hamim30 <hamimnasim30@gmail.com>
2022-09-28 20:51:19 +0600

    prime number task

--- src/PrimeNumber.java
+++ src/PrimeNumber.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class PrimeNumber {
+import java.util.Scanner;
+public class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner inputTake = new Scanner(System.in);
+        int number = inputTake.nextInt();
+        int checker=0;
+        for (int i =2; i<number;i++){
+            if (number %i==0){
+                System.out.println("Not a prime number");
+                checker=1;
+                break;
+            }
+        }
+        if (checker==0){
+            System.out.println("Prime number");
+        }
+    }
 }
